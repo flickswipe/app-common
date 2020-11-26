38f5468036be175ddc4540c488af5d0b1605e411
Chris Drifte <chrisdrifte@gmail.com>
2020-11-26 09:22:29 +0100

    Updates

--- src/middlewares/validate-iso6391-param.ts
+++ src/middlewares/validate-iso6391-param.ts
@@ -1,6 +1,6 @@
 import { param, ValidationChain } from "express-validator";
 
-export const validateIso6931Param = (field: string): ValidationChain =>
+export const validateIso6391Param = (field: string): ValidationChain =>
   param(field)
     .custom((value: string) => value.match(/[a-z]{2}/) != null)
     .withMessage(`must be valid language code`);
