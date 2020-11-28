70fe1d1e8d656d5353ca2166d44216918e05f54a
Chris Drifte <chrisdrifte@gmail.com>
2020-11-29 00:33:00 +0100

    Updates

--- src/events/media-item-updated.ts
+++ src/events/media-item-updated.ts
@@ -2,33 +2,33 @@ import { Subjects } from "./subjects";
 import { iso6391 } from "../iso6391";
 
 export interface MediaItemUpdatedEvent {
-    subject: Subjects.MediaItemUpdated;
-    data: {
-        id: string;
-        tmdbMovieId: number;
-        imdbId: string;
-        title: string;
-        genres: string[];
-        images: {
-            poster: string;
-            backdrop: string;
-        };
-        rating: {
-            average: number;
-            count: number;
-            popularity: number;
-        };
-        language: iso6391;
-        releaseDate: Date;
-        runtime: number;
-        plot: string;
-        streamLocations: {
-            [key: string]: {
-                id: string;
-                name: string;
-                url: string;
-            }[];
-        };
-        updatedAt: Date;
+  subject: Subjects.MediaItemUpdated;
+  data: {
+    id: string;
+    tmdbMovieId: number;
+    imdbId: string;
+    title: string;
+    genres: number[];
+    images: {
+      poster: string;
+      backdrop: string;
     };
+    rating: {
+      average: number;
+      count: number;
+      popularity: number;
+    };
+    language: iso6391;
+    releaseDate: Date;
+    runtime: number;
+    plot: string;
+    streamLocations: {
+      [key: string]: {
+        id: string;
+        name: string;
+        url: string;
+      }[];
+    };
+    updatedAt: Date;
+  };
 }
