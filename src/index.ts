e6409ad28e8ed73a3c1797e85bc9c134ff260269
Chris Drifte <developer@kolaps.com>
2020-12-10 11:47:09 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,7 @@ export * from "./startup/attach-exit-tasks";
 export * from "./startup/connect-to-database-server";
 export * from "./startup/connect-to-mail-server";
 export * from "./startup/connect-to-messaging-server";
+export * from "./startup/start-http-server";
 
 export * from "./utility/schedule-once";
 export * from "./utility/schedule-repeat";
